bce30882879037c3d0a0c274f07f7d1b0263bb43
Spencer Jack <spencer.jack@alaskaair.com>
2019-02-06 15:44:41 -0800

    adds JAWS.DB config connection

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Progress_19',
+        database: 'example_db'
+    });
+};
+
+connection.connect();
+module.exports = connection;
