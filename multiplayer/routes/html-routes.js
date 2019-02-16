9acd011b7b1333df798e371609fa34bb3bfc55e2
Lidet s <luk4ls@yahoo.com>
2019-02-15 20:24:56 -0800

    all in one

---
+++ multiplayer/routes/html-routes.js
@@ -0,0 +1,27 @@
+
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+    
+    app.get('/join', function(req, res){
+        res.sendFile(__dirname + '../views/join.html')
+    })
+    
+    app.get('/game', function(req, res){
+        gameQuery = req.query
+        getGameType(gameQuery)
+        res.sendFile(__dirname + '../views/game.html')
+    })
+    
+    
+    app.get('/', function(req, res){
+        res.sendFile(__dirname + '../views/index.html')
+    })
+    app.get('/Tournament', function(req, res){
+        gameQuery = req.query
+        getGameType(gameQuery)
+        res.sendFile(__dirname + '../views/Tournament.html')
+    })
+};
